25bbfb14f191992f407d957d524ffb1cd0378655
fujinhua <fu.jinhua@zte.com.cn>
2017-08-18 15:03:18 +0800

    Add seed codes of nfvo catalog
    Change-Id: Id0646870cbb297cbeddfe223feb2235f11a4a511
    Issue-Id: VFC-108
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

---
+++ catalog/pub/config/config.py
@@ -0,0 +1,60 @@
+# Copyright 2017 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+
+# [MSB]
+MSB_SERVICE_IP = '127.0.0.1'
+MSB_SERVICE_PORT = '80'
+
+# [REDIS]
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = '6379'
+REDIS_PASSWD = ''
+
+# [mysql]
+DB_IP = "127.0.0.1"
+DB_PORT = 3306
+DB_NAME = "nfvocatalog"
+DB_USER = "nfvocatalog"
+DB_PASSWD = "nfvocatalog"
+
+# [register]
+REG_TO_MSB_WHEN_START = True
+REG_TO_MSB_REG_URL = "/api/microservices/v1/services"
+REG_TO_MSB_REG_PARAM = {
+    "serviceName": "nfvocatalog",
+    "version": "v1",
+    "url": "/api/nfvocatalog/v1",
+    "protocol": "REST",
+    "visualRange": "1",
+    "nodes": [{
+        "ip": "127.0.0.1",
+        "port": "8806",
+        "ttl": 0
+    }]
+}
+
+# catalog path(values is defined in settings.py)
+CATALOG_ROOT_PATH = None
+CATALOG_URL_PATH = None
+
+# [sdc config]
+SDC_BASE_URL = "https://127.0.0.1:1234/api"
+SDC_USER = "admin"
+SDC_PASSWD = "admin"
+
+
+
+
+
---
+++ catalog/pub/msapi/sdc.py
@@ -0,0 +1,69 @@
+# Copyright 2017 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+
+from catalog.pub.exceptions import CatalogException
+from catalog.pub.utils import restcall
+from catalog.pub.config.config import SDC_BASE_URL, SDC_USER, SDC_PASSWD
+
+logger = logging.getLogger(__name__)
+
+ASSETTYPE_RESOURCES = "resources" 
+ASSETTYPE_SERVICES = "services"
+
+def call_sdc(resource, method, content=''):
+    return restcall.call_req(base_url=SDC_BASE_URL, 
+        user=SDC_USER, 
+        passwd=SDC_PASSWD, 
+        auth_type=restcall.rest_no_auth, 
+        resource=resource, 
+        method=method, 
+        content=content)
+
+def get_artifacts(asset_type):
+    resource = "/sdc/v1/catalog/{assetType}"
+    resource = resource.format(assetType=asset_type)
+    ret = call_sdc(resource, "GET")
+    if ret[0] != 0:
+        logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
+        raise CatalogException("Failed to query artifacts(%s) from sdc." % asset_type)
+    return json.JSONDecoder().decode(ret[1])
+
+def get_artifact(asset_type, csar_id):
+    artifacts = get_artifacts(asset_type)
+    for artifact in artifacts:
+        if artifact["uuid"] == csar_id:
+            return artifact
+    raise CatalogException("Failed to query artifact(%s,%s) from sdc." % (asset_type, csar_id))
+
+def delete_artifact(asset_type, asset_id, artifact_id):
+    resource = "/sdc/v1/catalog/{assetType}/{uuid}/artifacts/{artifactUUID}"
+    resource = resource.format(assetType=asset_type, uuid=asset_id, artifactUUID=artifact_id)
+    ret = call_sdc(resource, "DELETE")
+    if ret[0] != 0:
+        logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
+        raise CatalogException("Failed to delete artifacts(%s) from sdc." % artifact_id)
+    return json.JSONDecoder().decode(ret[1])
+
+def download_artifacts(download_url, local_path):
+    ret = restcall.call_req(base_url=download_url, 
+        user=SDC_USER, 
+        passwd=SDC_PASSWD, 
+        auth_type=rest_no_auth, 
+        resource="", 
+        method="GET")
+    # TODO:
+    
