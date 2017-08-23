4dbfe6e6a6fe69144154c22675232abeb954a083
fengyuanxing <feng.yuanxing@zte.com.cn>
2017-08-23 14:48:33 +0800

    Fix the bugs of tox.inn and add urls for catalog
    Change-Id: I87561fb83dde2eef58b6dc61c4c484a074ef3516
    Issue-Id: VFC-148
    Signed-off-by: fengyuanxing <feng.yuanxing@zte.com.cn>

--- catalog/packages/urls.py
+++ catalog/packages/urls.py
@@ -13,15 +13,10 @@
 # limitations under the License.
 
 from django.conf.urls import include, url
-from catalog.pub.config.config import REG_TO_MSB_WHEN_START, REG_TO_MSB_REG_URL, REG_TO_MSB_REG_PARAM
+from catalog.packages import views
 
 urlpatterns = [
-    url(r'^', include('catalog.samples.urls')),
-    url(r'^', include('catalog.packages.urls')),
+    url(r'^api/nfvocatalog/v1/packages/(?P<csarId>[a-zA-Z\-]+)$', views.package_get()),
 ]
 
-# regist to MSB when startup
-if REG_TO_MSB_WHEN_START:
-    import json
-    from catalog.pub.utils.restcall import req_by_msb
-    req_by_msb(REG_TO_MSB_REG_URL, "POST", json.JSONEncoder().encode(REG_TO_MSB_REG_PARAM))
+
