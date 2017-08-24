dabd05f5d490f32cd9e46f1b23476e7ffc6b640f
maopengzhang <zhang.maopeng1@zte.com.cn>
2017-08-24 19:46:56 +0800

    swagger supports for catalog
    Support swagger file in catalog
    
    Change-Id: Ieb2778fd4239cdae66d660f536f930e5d0011db1
    Issue-ID: VFC-145
    Signed-off-by: maopengzhang <zhang.maopeng1@zte.com.cn>

--- catalog/settings.py
+++ catalog/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'catalog.pub.database',
-    'catalog.samples'
+    'catalog.samples',
+    'catalog.swagger'
 ]
 
 MIDDLEWARE_CLASSES = [
