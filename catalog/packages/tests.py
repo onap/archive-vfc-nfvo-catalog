fe5fd27c1fb55eca78c6a9f17c86dff3188c3eba
fengyuanxing <feng.yuanxing@zte.com.cn>
2017-08-26 20:11:31 +0800

    Add code related to sdc
    Change-Id: If4ddb9c10e654b04b033b63005f99ddf1adc599b
    Issue-Id: VFC-152
    Signed-off-by: fengyuanxing <feng.yuanxing@zte.com.cn>

--- catalog/packages/tests.py
+++ catalog/packages/tests.py
@@ -21,15 +21,46 @@ from rest_framework import status
 class PackageTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
+        self.nsdata = None
+        self.nfdata = None
+        self.ns_csarId = 123
+        self.nf_csarId = 456
+
+        self.nsdata = {
+            "csarId": self.ns_csarId
+        }
+
+        self.nfdata = {
+            "csarId": self.nf_csarId
+        }
+
 
     def tearDown(self):
         pass
 
     def test_nspackage_get(self):
-
         response = self.client.get("/api/nfvocatalog/v1/nspackages")
-        #self.assertEqual(status.HTTP_200_OK, response.status_code, response.content)
+        self.assertEqual(status.HTTP_200_OK, response.status_code, response.content)
 
     def test_nfpackage_get(self):
-        response = self.client.get("/api/nfvocatalog/v1/nfpackages")
+        response = self.client.get("/api/nfvocatalog/v1/vnfpackages")
+        self.assertEqual(status.HTTP_200_OK, response.status_code, response.content)
+
+    def test_ns_distribute(self):
+        response = self.client.post("/api/nfvocatalog/v1/nspackages",self.nsdata)
         #self.assertEqual(status.HTTP_200_OK, response.status_code, response.content)
+
+
+    def test_nf_distribute(self):
+        response = self.client.post("/api/nfvocatalog/v1/vnfpackages",self.nfdata)
+        #self.assertEqual(status.HTTP_202_ACCEPTED, response.status_code, response.content)
+        
+
+    def test_ns_package_delete(self):
+        response = self.client.delete("/api/nfvocatalog/v1/nspackages/" + str(self.ns_csarId))
+        self.assertEqual(status.HTTP_202_ACCEPTED, response.status_code, response.content)
+
+    def test_nf_package_delete(self):
+        #response = self.client.delete("/api/nfvocatalog/v1/vnfpackages/" + str(self.nf_csarId))
+        #self.assertEqual(status.HTTP_202_ACCEPTED, response.status_code, response.content)
+        pass
