87990c8fe619d0fd9248ebe44ecaa74963606786
fengyuanxing <feng.yuanxing@zte.com.cn>
2018-01-25 15:08:34 +0800

    Add validataion code of swagger code
    Change-Id: I5e829afc6fe55ccfc44b8ef8d2fae9db888f28ac
    Issue-ID: VFC-677
    Signed-off-by: fengyuanxing <feng.yuanxing@zte.com.cn>

--- catalog/jobs/tests/tests.py
+++ catalog/jobs/tests/tests.py
@@ -27,7 +27,7 @@ class JobsViewTest(TestCase):
 
     def test_job_normal(self):
         JobModel(jobid=self.job_id, jobtype='VNF', jobaction='INST', resid='1').save()
-        JobStatusModel(indexid=1, jobid=self.job_id, status='inst', progress=20, descp='inst').save()
+        JobStatusModel(indexid=1, jobid=self.job_id, status='inst', errcode='0', progress=20, descp='inst').save()
         response = self.client.get("/api/catalog/v1/jobs/%s" % self.job_id)
         self.failUnlessEqual(status.HTTP_200_OK, response.status_code)
 
