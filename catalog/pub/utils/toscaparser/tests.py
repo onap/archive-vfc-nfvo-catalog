a499e299107b3776d0e32c88e545940e94cb0a08
Huang Haibin <haibin.huang@intel.com>
2018-04-13 15:54:59 +0800

    Add vCPE csar test file
    Change-Id: I4eb0d19674ae245f51fc88b1031fc684b58800e7
    Issue-ID: VFC-875
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- catalog/pub/utils/toscaparser/tests.py
+++ catalog/pub/utils/toscaparser/tests.py
@@ -13,10 +13,13 @@
 # limitations under the License.
 import json
 import os
+import logging
 
 from django.test import TestCase
 
 from catalog.pub.utils.toscaparser import parse_vnfd
+
+logger = logging.getLogger(__name__)
 
 
 class TestToscaparser(TestCase):
@@ -29,6 +32,19 @@ class TestToscaparser(TestCase):
     def test_nsd_parse(self):
         csar_path = os.path.dirname(os.path.abspath(__file__)) + "/testdata/resource-ZteMmeFixVl-csar.csar"
         input_parameters = [{"value": "111111", "key": "sdncontroller"}]
+        logger.debug("csar_path:%s", csar_path)
         vnfd_json = parse_vnfd(csar_path, input_parameters)
         metadata = json.loads(vnfd_json).get("metadata")
         self.assertEqual("ZTE-MME-FIX-VL", metadata.get("name", ""))
+
+    def test_vcpe_parse(self):
+        csar_path = os.path.dirname(os.path.abspath(__file__)) + "/testdata/vcpe"
+        input_parameters = [{"value": "222222", "key": "sdncontroller"}]
+        vcpe = ["infra", "vbng", "vbrgemu", "vgmux", "vgw"]
+        for vcpe_part in vcpe:
+            csar_file = ("%s/%s.csar" % (csar_path, vcpe_part))
+            logger.debug("csar_file:%s", csar_file)
+            vnfd_json = parse_vnfd(csar_file, input_parameters)
+            metadata = json.loads(vnfd_json).get("metadata")
+            logger.debug("metadata:%s", metadata)
+            self.assertEqual(("vCPE_%s" % vcpe_part), metadata.get("template_name", ""))
