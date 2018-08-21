23a205759c59078fe4482809c9504920a3ae50a3
laili <lai.li@zte.com.cn>
2018-08-21 19:28:46 +0800

    Ns descriptor related stuffs.
    Add a urlpattern.
    Add a test to the creation of pnfd.
    
    Change-Id: I3b320db4d9b1f2f0cf0208590494bbf30b7b6289
    Issue-ID: VFC-1037
    Signed-off-by: laili <lai.li@zte.com.cn>

---
+++ catalog/packages/tests/test_pnfd.py
@@ -0,0 +1,61 @@
+# Copyright 2017 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class TestPnfDescriptor(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def tearDown(self):
+        pass
+
+    def test_pnfd_create_normal(self):
+        reqest_data = {
+            'userDefinedData': {
+                'key1': 'value1',
+                'key2': 'value2',
+                'key3': 'value3',
+            }
+        }
+        expected_reponse_data = {
+            'pnfdOnboardingState': 'CREATED',
+            'pnfdUsageState': 'NOT_IN_USE',
+            'userDefinedData': {
+                'key1': 'value1',
+                'key2': 'value2',
+                'key3': 'value3',
+            },
+            '_links': None
+        }
+        response = self.client.post(
+            '/api/nsd/v1/pnf_descriptors',
+            data=reqest_data,
+            format='json'
+        )
+        response.data.pop('id')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(expected_reponse_data, response.data)
+
+    def test_pnfd_content_upload_normal(self):
+        pass
+
+    def test_pnfd_content_upload_failure(self):
+        pass
