8164b299e0f6f508505e357c2552cac5558f1857
laili <lai.li@zte.com.cn>
2018-08-22 18:52:02 +0800

    Ns descriptor related stuffs.
    Modify the biz for uploading of nsd content.
    Modify some file names.
    
    Change-Id: I7ecaef1bfbf8a8c10c2fee8bc70f6ab3bfb7485d
    Issue-ID: VFC-1037
    Signed-off-by: laili <lai.li@zte.com.cn>

---
+++ catalog/packages/tests/test_ns_descriptor.py
@@ -0,0 +1,228 @@
+# Copyright 2017 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import json
+import os
+
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from catalog.pub.database.models import NSPackageModel
+
+
+class TestNsDescriptor(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def tearDown(self):
+        pass
+
+    def test_nsd_create_normal(self):
+        reqest_data = {
+            'userDefinedData': {
+                'key1': 'value1',
+                'key2': 'value2',
+                'key3': 'value3',
+            }
+        }
+        expected_reponse_data = {
+            'nsdOnboardingState': 'CREATED',
+            'nsdOperationalState': 'DISABLED',
+            'nsdUsageState': 'NOT_IN_USE',
+            'userDefinedData': {
+                'key1': 'value1',
+                'key2': 'value2',
+                'key3': 'value3',
+            },
+            '_links': None
+        }
+        response = self.client.post(
+            '/api/nsd/v1/ns_descriptors',
+            data=reqest_data,
+            format='json'
+        )
+        response.data.pop('id')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(expected_reponse_data, response.data)
+
+    def test_query_multiple_nsds_normal(self):
+        expected_reponse_data = [
+            {
+                'id': '0',
+                'nsdId': None,
+                'nsdName': None,
+                'nsdVersion': None,
+                'nsdDesigner': None,
+                'nsdInvariantId': None,
+                'vnfPkgIds': [],
+                'pnfdInfoIds': [],
+                'nestedNsdInfoIds': [],
+                'nsdOnboardingState': 'CREATED',
+                'onboardingFailureDetails': None,
+                'nsdOperationalState': 'DISABLED',
+                'nsdUsageState': 'NOT_IN_USE',
+                'userDefinedData': {
+                    'key1': 'value1',
+                    'key2': 'value2',
+                    'key3': 'value3',
+                },
+                '_links': None
+            },
+            {
+                'id': '1',
+                'nsdId': None,
+                'nsdName': None,
+                'nsdVersion': None,
+                'nsdDesigner': None,
+                'nsdInvariantId': None,
+                'vnfPkgIds': [],
+                'pnfdInfoIds': [],
+                'nestedNsdInfoIds': [],
+                'nsdOnboardingState': 'CREATED',
+                'onboardingFailureDetails': None,
+                'nsdOperationalState': 'DISABLED',
+                'nsdUsageState': 'NOT_IN_USE',
+                'userDefinedData': {
+                    'key1': 'value1',
+                    'key2': 'value2',
+                    'key3': 'value3',
+                },
+                '_links': None
+            }
+        ]
+        user_defined_data = {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+        }
+        user_defined_data = json.JSONEncoder().encode(user_defined_data)
+        for i in range(2):
+            NSPackageModel(
+                nsPackageId=str(i),
+                operationalState='DISABLED',
+                usageState='NOT_IN_USE',
+                userDefinedData=user_defined_data
+            ).save()
+        response = self.client.get('/api/nsd/v1/ns_descriptors', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(expected_reponse_data, response.data)
+
+    def test_query_single_nsd_normal(self):
+        expected_reponse_data = {
+            'id': '22',
+            'nsdId': None,
+            'nsdName': None,
+            'nsdVersion': None,
+            'nsdDesigner': None,
+            'nsdInvariantId': None,
+            'vnfPkgIds': [],
+            'pnfdInfoIds': [],
+            'nestedNsdInfoIds': [],
+            'nsdOnboardingState': 'CREATED',
+            'onboardingFailureDetails': None,
+            'nsdOperationalState': 'DISABLED',
+            'nsdUsageState': 'NOT_IN_USE',
+            'userDefinedData': {
+                'key1': 'value1',
+                'key2': 'value2',
+                'key3': 'value3',
+            },
+            '_links': None
+        }
+        user_defined_data = {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+        }
+        user_defined_data = json.JSONEncoder().encode(user_defined_data)
+        NSPackageModel(
+            nsPackageId='22',
+            operationalState='DISABLED',
+            usageState='NOT_IN_USE',
+            userDefinedData=user_defined_data
+        ).save()
+
+        response = self.client.get('/api/nsd/v1/ns_descriptors/22', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(expected_reponse_data, response.data)
+
+    def test_delete_single_nsd_normal(self):
+        user_defined_data = {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+        }
+        user_defined_data = json.JSONEncoder().encode(user_defined_data)
+        NSPackageModel(
+            nsPackageId='22',
+            operationalState='DISABLED',
+            usageState='NOT_IN_USE',
+            userDefinedData=user_defined_data,
+            nsdModel='test'
+        ).save()
+        resp = self.client.delete("/api/nsd/v1/ns_descriptors/22", format='json')
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual({}, resp.data)
+
+    def test_nsd_content_upload_normal(self):
+        user_defined_data = {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+        }
+        user_defined_data = json.JSONEncoder().encode(user_defined_data)
+        NSPackageModel(
+            nsPackageId='22',
+            operationalState='DISABLED',
+            usageState='NOT_IN_USE',
+            userDefinedData=user_defined_data,
+        ).save()
+        with open('nsd_content.txt', 'wb') as fp:
+            fp.write('test')
+
+        with open('nsd_content.txt', 'rb') as fp:
+            resp = self.client.put(
+                "/api/nsd/v1/ns_descriptors/22/nsd_content",
+                {'file': fp},
+            )
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual({}, resp.data)
+
+        os.remove('nsd_content.txt')
+
+    def test_nsd_content_upload_normal(self):
+        user_defined_data = {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+        }
+        user_defined_data = json.JSONEncoder().encode(user_defined_data)
+        NSPackageModel(
+            nsPackageId='22',
+            operationalState='DISABLED',
+            usageState='NOT_IN_USE',
+            userDefinedData=user_defined_data,
+        ).save()
+
+        with open('D:\\onap\\src\\catalog\\catalog\\packages\\tests\\data\\vIMS_v2.csar', 'rb') as fp:
+            resp = self.client.put(
+                "/api/nsd/v1/ns_descriptors/22/nsd_content",
+                {'file': fp},
+            )
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual({}, resp.data)
+
+    def test_nsd_content_upload_failure(self):
+        pass
