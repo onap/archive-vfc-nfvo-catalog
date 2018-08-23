bf6cb8aaf21802d03292b5c24e9b54480da5feb7
laili <lai.li@zte.com.cn>
2018-08-23 11:28:27 +0800

    Ns descriptor related stuffs.
    Add tests for creation of pnfd.
    Modify names.
    
    Change-Id: I915f561f895b9d5a033b12ba3f5dcdc350812580
    Issue-ID: VFC-1037
    Signed-off-by: laili <lai.li@zte.com.cn>

---
+++ catalog/packages/tests/test_pnf_descriptor.py
@@ -0,0 +1,80 @@
+# Copyright 2017 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class TestPnfDescriptor(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def tearDown(self):
+        pass
+
+    def test_pnfd_create_normal(self):
+        reqest_data = {
+            'userDefinedData': {
+                'key1': 'value1',
+                'key2': 'value2',
+                'key3': 'value3',
+            }
+        }
+        expected_reponse_data = {
+            'pnfdOnboardingState': 'CREATED',
+            'pnfdUsageState': 'NOT_IN_USE',
+            'userDefinedData': {
+                'key1': 'value1',
+                'key2': 'value2',
+                'key3': 'value3',
+            },
+            '_links': None
+        }
+        response = self.client.post(
+            '/api/nsd/v1/pnf_descriptors',
+            data=reqest_data,
+            format='json'
+        )
+        response.data.pop('id')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(expected_reponse_data, response.data)
+
+    def test_query_multiple_pnfds_normal(self):
+        pass
+
+    def test_query_single_pnfd_normal(self):
+        pass
+
+    def test_delete_single_pnfd_normal(self):
+        pass
+
+    def test_pnfd_content_upload_normal(self):
+        with open('pnfd_content.txt', 'wb') as fp:
+            fp.write('test')
+
+        with open('pnfd_content.txt', 'rb') as fp:
+            resp = self.client.put(
+                "/api/nsd/v1/pnf_descriptors/22/pnfd_content",
+                {'file': fp},
+            )
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual({}, resp.data)
+
+        os.remove('pnfd_content.txt')
+
+    def test_pnfd_content_upload_failure(self):
+        pass
