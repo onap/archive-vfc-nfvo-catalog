e96d2314d690548b494f60648827b08bc0441b0e
biancunkang <bian.cunkang@zte.com.cn>
2018-08-23 11:15:43 +0800

    Deal with nfPackage
    Modify url and delete functions in old files
    
    Change-Id: Ie90c59fda9f73335afee61eb7090a57ccdc0e04e
    Issue-ID: VFC-1038
    Signed-off-by: biancunkang <bian.cunkang@zte.com.cn>

--- catalog/packages/biz/vnfpackage.py
+++ catalog/packages/biz/vnfpackage.py
@@ -29,7 +29,6 @@ from catalog.pub.msapi import sdc
 from catalog.pub.utils import fileutil
 from catalog.pub.utils import toscaparser
 from catalog.pub.utils.jobutil import JobUtil
-from catalog.pub.utils.values import ignore_case_get
 
 logger = logging.getLogger(__name__)
 
@@ -77,40 +76,6 @@ def parse_vnfd(csar_id, inputs):
         logger.error(traceback.format_exc())
         return [1, str(sys.exc_info())]
     return [0, ret]
-
-
-def create_vnf_pkg(data):
-    user_defined_data = ignore_case_get(data, "userDefinedData")
-    vnfPkgId = str(uuid.uuid4())
-    VnfPackageModel.objects.create(
-        vnfPackageId=vnfPkgId,
-        onboardingState="CREATED",
-        operationalState="DISABLED",
-        usageState="NOT_IN_USE",
-        userDefinedData=user_defined_data
-    )
-    data = {
-        "id": vnfPkgId,
-        "onboardingState": "CREATED",
-        "operationalState": "DISABLED",
-        "usageState": "NOT_IN_USE",
-        "userDefinedData": user_defined_data,
-        "_links": None
-    }
-    return data
-
-
-def query_multiple():
-    # TODO
-    data = {
-        "id": "1",
-        "onboardingState": "CREATED",
-        "operationalState": "DISABLED",
-        "usageState": "NOT_IN_USE",
-        "userDefinedData": "1",
-        "_links": None
-    }
-    return data
 
 
 class NfDistributeThread(threading.Thread):
@@ -230,38 +195,6 @@ class NfPkgDeleteThread(threading.Thread):
         fileutil.delete_dirs(csar_save_path)
 
         JobUtil.add_job_status(self.job_id, 100, "Delete CSAR(%s) successfully." % self.csar_id)
-
-
-class VnfpkgUploadThread(threading.Thread):
-    def __init__(self, data, vnfPkgId):
-        threading.Thread.__init__(self)
-        self.vnfPkgId = vnfPkgId
-        self.data = data
-
-    def run(self):
-        try:
-            self.upload_vnfPkg_from_uri()
-        except CatalogException as e:
-            logger.error(e.message)
-        except Exception as e:
-            logger.error(e.message)
-            logger.error(traceback.format_exc())
-            logger.error(str(sys.exc_info()))
-
-    def upload_vnfPkg_from_uri(self):
-        logger.debug("UploadVnf %s" % self.vnfPkgId)
-        uri = ignore_case_get(self.data, "addressInformation")
-        upload_path = os.path.join(CATALOG_ROOT_PATH, self.vnfPkgId)
-        if not os.path.exists(upload_path):
-            os.makedirs(upload_path, 0o777)
-        r = urllib2.Request(uri)
-        req = urllib2.urlopen(r)
-
-        upload_file_name = os.path.join(upload_path, os.path.basename(uri))
-        save_file = open(upload_file_name, "wb")
-        save_file.write(req.read())
-        save_file.close()
-        req.close()
 
 
 class NfPackage(object):
