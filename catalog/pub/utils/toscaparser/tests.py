840ebc3759316a68bd7e9d8672439a126398be30
maopengzhang <zhang.maopeng1@zte.com.cn>
2018-09-30 14:18:16 +0800

    add sdc service csar related to PNF
    add sdc service csar related to PNF
    
    Change-Id: I2586fd2397ad9799e2b9a229fcb6117135bf8a94
    Issue-ID: VFC-1102
    Signed-off-by: maopengzhang <zhang.maopeng1@zte.com.cn>

--- catalog/pub/utils/toscaparser/tests.py
+++ catalog/pub/utils/toscaparser/tests.py
@@ -20,6 +20,7 @@ import shutil
 from django.test import TestCase
 
 from catalog.pub.utils.toscaparser import parse_vnfd, parse_pnfd, parse_nsd
+from catalog.pub.utils.toscaparser.graph import Graph
 
 logger = logging.getLogger(__name__)
 
@@ -31,11 +32,11 @@ class TestToscaparser(TestCase):
     def tearDown(self):
         pass
 
-    def test_vnf_parse(self):
+    def test_vnfd_parse(self):
         self.remove_temp_dir()
         csar_path = os.path.dirname(os.path.abspath(__file__)) + "/testdata/vnf"
         input_parameters = [{"value": "222222", "key": "sdncontroller"}]
-        vcpe = ["infra", "vbng", "vbrgemu", "vgmux", "vgw"]
+        vcpe = ["vgw"]
         for vcpe_part in vcpe:
             csar_file = ("%s/%s.csar" % (csar_path, vcpe_part))
             logger.debug("csar_file:%s", csar_file)
@@ -51,22 +52,21 @@ class TestToscaparser(TestCase):
         metadata = json.loads(pnfd_json).get("metadata")
         self.assertEqual("RAN_DU", metadata.get("template_name", ""))
 
+    """
     def test_nsd_parse(self):
         self.remove_temp_dir()
-        pnf_csar = os.path.dirname(os.path.abspath(__file__)) + "/testdata/pnf/ran-du.csar"
-        nsd_json = parse_nsd(pnf_csar)
+        ran_csar = os.path.dirname(os.path.abspath(__file__)) + "/testdata/ns/ran.csar"
+        nsd_json = parse_nsd(ran_csar)
         metadata = json.loads(nsd_json).get("metadata")
-        self.assertNotEqual("RAN-NS", metadata.get("template_name", ""))
-
-        pnf_csar = os.path.dirname(os.path.abspath(__file__)) + "/testdata/vnf/vgw.csar"
-        nsd_json = parse_nsd(pnf_csar)
-        metadata = json.loads(nsd_json).get("metadata")
-        self.assertNotEqual("RAN-NS", metadata.get("template_name", ""))
-
-        # ran_csar = os.path.dirname(os.path.abspath(__file__)) + "/testdata/ns/ran.csar"
-        # nsd_json = parse_nsd(ran_csar)
-        # metadata = json.loads(nsd_json).get("metadata")
-        # self.assertEqual("RAN-NS", metadata.get("template_name", ""))
+        self.assertEqual("RAN-NS", metadata.get("template_name", ""))
+   """
+    
+    def test_service_descriptor_parse(self):
+        self.remove_temp_dir()
+        service_test_csar = os.path.dirname(os.path.abspath(__file__)) + "/testdata/ns/service-vIMS.csar"
+        test_json = parse_nsd(service_test_csar, [], False)
+        metadata = json.loads(test_json).get("metadata")
+        self.assertEqual("vIMS_v2", metadata.get("name", ""))
 
     def remove_temp_dir(self):
         tempdir = tempfile.gettempdir()
@@ -75,3 +75,14 @@ class TestToscaparser(TestCase):
                 path = tempfile.tempdir + "/" + dir
                 if (not os.path.isfile(path)) and os.path.exists(path):
                     shutil.rmtree(tempfile.tempdir + "/" + dir)
+
+    def test_graph(self):
+        data = {
+            "cucp": [],
+            "du": [],
+            "vl_flat_net": ["cucp", "cuup"],
+            "vl_ext_net": ["cucp", "cuup"],
+            "cuup": []
+        }
+        graph = Graph(data)
+        self.assertEquals(['vl_ext_net', 'vl_flat_net'], graph.get_pre_nodes("cucp"))
