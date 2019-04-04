8cf5ec4adee3868174f1a7c2735a7fca8992f498
yangyan <yangyanyj@chinamobile.com>
2019-04-04 11:04:38 +0800

    Integrate uwsgi with django
    Change-Id: I600f42489c95a54b8a70ca1978d6b039fe945ebe
    Issue-ID: VFC-1336
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- stop.sh
+++ stop.sh
@@ -13,4 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ps auxww | grep "manage.py runserver 0.0.0.0:8806" | awk '{print $2}' | xargs kill -9
+# ps auxww | grep "manage.py runserver 0.0.0.0:8806" | awk '{print $2}' | xargs kill -9
+ps auxww |grep 'uwsgi --http :8806 --module lcm.wsgi --master' |awk '{print $2}' |xargs kill -9
