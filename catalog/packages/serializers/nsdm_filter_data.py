7bf3c0f9eb53bd37980d62c72612b4c39a1c6029
fujinhua <fu.jinhua@zte.com.cn>
2019-04-06 08:43:50 +0800

    Update serializer for nsdm filter
    Change-Id: Ice220e3b869e3b1b2aec86cedbf37a19f1ade40a
    Issue-ID: VFC-1306
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- catalog/packages/serializers/nsdm_filter_data.py
+++ catalog/packages/serializers/nsdm_filter_data.py
@@ -21,104 +21,157 @@ class NsdmNotificationsFilter(serializers.Serializer):
     notificationTypes = serializers.ListField(
         child=serializers.ChoiceField(
             required=True,
-            choices=NSDM_NOTIFICATION_TYPES),
+            choices=NSDM_NOTIFICATION_TYPES
+        ),
         help_text="Match particular notification types",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nsdInfoId = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match NS packages with particular nsdInfoIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nsdId = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match NS Packages with particular nsdIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nsdName = serializers.ListField(
-        child=serializers.CharField(max_length=255, required=True),
+        child=serializers.CharField(
+            max_length=255,
+            required=True
+        ),
         help_text="Match NS Packages with particular nsdNames",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nsdVersion = serializers.ListField(
-        child=serializers.CharField(max_length=255, required=True),
+        child=serializers.CharField(
+            max_length=255,
+            required=True
+        ),
         help_text="match NS packages that belong to certain nsdversion",
         required=False,
-        allow_null=False)
+        allow_null=False
+    )
     nsdInvariantId = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match NS Packages with particular nsdInvariantIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     vnfPkgIds = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match NS Packages that has VNF PackageIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nestedNsdInfoIds = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match NS Packages with particular nsdInvariantIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nsdOnboardingState = serializers.ListField(
-        child=serializers.ChoiceField(required=True,
-                                      choices=['CREATED', 'UPLOADING',
-                                               'PROCESSING', 'ONBOARDED']),
+        child=serializers.ChoiceField(
+            required=True,
+            choices=[
+                'CREATED',
+                'UPLOADING',
+                'PROCESSING',
+                'ONBOARDED'
+            ]
+        ),
         help_text="Match NS Packages with particular NS Onboarding State",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nsdOperationalState = serializers.ListField(
-        child=serializers.ChoiceField(required=True,
-                                      choices=['ENABLED', 'DISABLED']),
+        child=serializers.ChoiceField(
+            required=True,
+            choices=['ENABLED', 'DISABLED']
+        ),
         help_text="Match NS Packages with particular NS Operational State",
         allow_null=False,
-        required=False)
+        required=False
+    )
     nsdUsageState = serializers.ListField(
-        child=serializers.ChoiceField(required=True,
-                                      choices=['IN_USE', 'NOT_IN_USE']),
+        child=serializers.ChoiceField(
+            required=True,
+            choices=['IN_USE', 'NOT_IN_USE']
+        ),
         help_text="Match NS Packages with particular NS Usage State",
         allow_null=False,
-        required=False)
+        required=False
+    )
     pnfdInfoIds = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match PF packages with particular pnfdInfoIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     pnfdId = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match PF packages with particular pnfdInfoIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     pnfdName = serializers.ListField(
-        child=serializers.CharField(max_length=255, required=True),
+        child=serializers.CharField(
+            max_length=255,
+            required=True
+        ),
         help_text="Match PF Packages with particular pnfdNames",
         allow_null=False,
-        required=False)
+        required=False
+    )
     pnfdVersion = serializers.ListField(
-        child=serializers.CharField(max_length=255, required=True),
+        child=serializers.CharField(
+            max_length=255,
+            required=True
+        ),
         help_text="match PF packages that belong to certain pnfd version",
         required=False,
-        allow_null=False)
+        allow_null=False
+    )
     pnfdProvider = serializers.ListField(
-        child=serializers.CharField(max_length=255, required=True),
+        child=serializers.CharField(
+            max_length=255,
+            required=True
+        ),
         help_text="Match PF Packages with particular pnfdProvider",
         allow_null=False,
-        required=False)
+        required=False
+    )
     pnfdInvariantId = serializers.ListField(
         child=serializers.UUIDField(),
         help_text="Match PF Packages with particular pnfdInvariantIds",
         allow_null=False,
-        required=False)
+        required=False
+    )
     pnfdOnboardingState = serializers.ListField(
-        child=serializers.ChoiceField(required=True,
-                                      choices=['CREATED', 'UPLOADING',
-                                               'PROCESSING', 'ONBOARDED']),
+        child=serializers.ChoiceField(
+            required=True,
+            choices=[
+                'CREATED',
+                'UPLOADING',
+                'PROCESSING',
+                'ONBOARDED'
+            ]
+        ),
         help_text="Match PF Packages with particular PNF Onboarding State ",
         allow_null=False,
-        required=False)
+        required=False
+    )
     pnfdUsageState = serializers.ListField(
         child=serializers.ChoiceField(
-            required=True, choices=['IN_USE', 'NOT_IN_USE']),
+            required=True,
+            choices=['IN_USE', 'NOT_IN_USE']
+        ),
         help_text="Match PF Packages with particular PNF usage State",
         allow_null=False,
-        required=False)
+        required=False
+    )
