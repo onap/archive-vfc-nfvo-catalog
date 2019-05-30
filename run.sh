aac59835976fe940f9c07ddaa792d5661bfbdc8f
tianxing <15210838572@139.com>
2019-05-30 19:56:05 +0800

    change the way to start django
    Change-Id: Ic117d718155faf6e6b098efd9c83b8eb6aced2a7
    Issue-ID: VFC-1408
    Signed-off-by: tianxing <15210838572@139.com>

--- run.sh
+++ run.sh
@@ -18,12 +18,12 @@ if [ ! -x  $logDir  ]; then
     mkdir -p $logDir
 fi
 
-# nohup python manage.py runserver 0.0.0.0:8806 > /dev/null &
+nohup python manage.py runserver 0.0.0.0:8806 > /dev/null &
 # nohup uwsgi --http :8806 -t 120 --module catalog.wsgi --master --processes 4 &
 
 
-if [ "${SSL_ENABLED}" = "true" ]; then
-    nohup uwsgi --https :8806,catalog/pub/ssl/cert/foobar.crt,catalog/pub/ssl/cert/foobar.key, -t 120 --enable-threads --module catalog.wsgi --master --processes 4 &
-else
-    nohup uwsgi --http :8806 -t 120 --enable-threads --module catalog.wsgi --master --processes 4 &
-fi
+# if [ "${SSL_ENABLED}" = "true" ]; then
+#     nohup uwsgi --https :8806,catalog/pub/ssl/cert/foobar.crt,catalog/pub/ssl/cert/foobar.key, -t 120 --enable-threads --module catalog.wsgi --master --processes 4 &
+# else
+#     nohup uwsgi --http :8806 -t 120 --enable-threads --module catalog.wsgi --master --processes 4 &
+# fi
